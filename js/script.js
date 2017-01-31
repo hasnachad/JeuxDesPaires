faa359f87101dbaa9bc915a3879852892384d6cd
hasna Chaddou <hasnachad@gmail.com>
2017-01-31 01:51:06 +0100

    Js commenté

--- js/script.js
+++ js/script.js
@@ -1,23 +1,21 @@
-/*déclaration du tableau d'images
-var tabImages = ['img/ane.jpg', 'img/chat.jpg', 'img/hien.jpg', 'img/lama.jpg', 'img/lapins.jpg', 'img/lionne.jpg', 'img/ours.jpg'];*/
-alert('test');
+//alert('test'); Vérifie le script avec le html.
 
 //Déclaration d'un nouveau tableau
 var mesCases = new Array();
 //je parcours mon tableau 
 for (i=0; i<=6;i++){
-	mesCases.push(i,i);
+	mesCases.push(i,i); //je remplies mon tableau avec mes images en double
 }
 //Function qui prend en paramètre un tableau
 // et le tri dans un ordre aléatoire puis le retourne
 function shuffle(a){
-   var j = 0;
+   var j = 0; 
    var valI = '';
    var valJ = valI;
    var l = a.length - 1;
 
    while(l > -1){
-		j = Math.floor(Math.random() * l);
+		j = Math.floor(Math.random() * l);// renvoie le > entier qui <= un nombre x.
 		valI = a[l];
 		valJ = a[j];
 		a[l] = valJ;
@@ -31,6 +29,6 @@ function shuffle(a){
 shuffle(mesCases);
  for (i=0; i<14; i++){
  	//document html qui va chercher l'element qui a l'id paires dans le dossier img/img.jpg
- 	document.getElementById('paires').innerHTML+='<img src="img/img'+mesCases[i]+'.jpg" alt=""/>';
+ 	document.getElementById('paires').innerHTML+='<img onclick="" src="img/img'+mesCases[i]+'.jpg" alt=""/>';
  }
 
