4e9832a620f9c0173302555d631f91c491ca5316
hasna Chaddou <hasnachad@gmail.com>
2017-01-31 01:02:43 +0100

    Function de mélange des cartes et affichage

--- js/script.js
+++ js/script.js
@@ -1,28 +1,36 @@
-//déclaration du tableau d'images
-var tabImages = ['img/ane.jpg', 'img/chat.jpg', 'img/hien.jpg', 'img/lama.jpg', 'img/lapins.jpg', 'img/lionne.jpg', 'img/ours.jpg'];
+/*déclaration du tableau d'images
+var tabImages = ['img/ane.jpg', 'img/chat.jpg', 'img/hien.jpg', 'img/lama.jpg', 'img/lapins.jpg', 'img/lionne.jpg', 'img/ours.jpg'];*/
+alert('test');
 
-
-/*
-function AfficherImages(){
-	document.getElementById('images').innerHTML="";//document html qui va chercher l'element qui a l'id images
-	for (var i = 0; i < tabImage.lenght -1; i++) {   
-		var lignetab='<tr>';//ouverture de la ligne, declaration de ma nouvelle ligne
-		lignetab+='<td>';//ajoute une colonne à ma ligne
-		lignetab+=tabImages[i]; //ajoute dans la console l'image correspondante.
-		lignetab+='</td>';
-		//document.getElementById('classement').inner.HTML+='<td>coucou</td>';
-			console.log(tabImages[i]);
-		}
-		lignetab+='</tr>';
-		//alert(); affichage ligne du tableau
-		document.getElementById('images').innerHTML+=lignetab;//document html qui va chercher l'element
-																//qui a l'id images 
-	}
-*/
-
-for(var i = 0; i < document.tabImages.length; i++){
-  alert(document.tabImages[i].src)
+//Déclaration d'un nouveau tableau
+var mesCases = new Array();
+//je parcours mon tableau 
+for (i=0; i<=6;i++){
+	mesCases.push(i,i);
 }
+//Function qui prend en paramètre un tableau
+// et le tri dans un ordre aléatoire puis le retourne
+function shuffle(a){
+   var j = 0;
+   var valI = '';
+   var valJ = valI;
+   var l = a.length - 1;
 
+   while(l > -1){
+		j = Math.floor(Math.random() * l);
+		valI = a[l];
+		valJ = a[j];
+		a[l] = valJ;
+		a[j] = valI;
+		l = l - 1;
+	}
+	return a;
+ }
 
+//Appel de la fonction
+shuffle(mesCases);
+ for (i=0; i<14; i++){
+ 	//document html qui va chercher l'element qui a l'id paires dans le dossier img/img.jpg
+ 	document.getElementById('paires').innerHTML+='<img src="img/img'+mesCases[i]+'.jpg" alt=""/>';
+ }
 
